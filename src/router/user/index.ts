6c2675774a9ddaa14d25ed175c8d2d7c6d7a221a
Dongmin Cho <30872915+dmcho84@users.noreply.github.com>
2022-03-06 20:57:02 +0900

    로그인 구현

--- src/router/user/index.ts
+++ src/router/user/index.ts
@@ -1,10 +1,22 @@
+require('dotenv').config();
+
 import { Router } from 'express';
 import db from '../../models';
+import passport from 'passport';
+import { mustLogedIn } from '../../passport';
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
+
 const router = Router();
 
 // router.use('/', (req, res, next) => {
 //   res.send('user!!!');
 // });
+
+router.get('/', (req, res, next) => {
+  console.log(req.user);
+  res.json(req.user);
+});
 
 router.get('/all', async (req, res, next) => {
   db.User.findAll({
@@ -38,6 +50,65 @@ router.get('/all', async (req, res, next) => {
   //   });
   //   console.log(users.get());
   //   res.send = users.dataValues;
+});
+
+interface IUser {
+  name: string;
+  email: string;
+  password: string;
+}
+
+router.post('/signup', async (req, res, next) => {
+  try {
+    const { name, email, password }: IUser = req.body;
+    const hash = bcrypt.hashSync(password, 10);
+    await db.User.create({
+      name,
+      email,
+      password: hash,
+    }).then((user: any) => res.json(user));
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+});
+
+router.post('/signin', (req, res, next) => {
+  try {
+    // 아까 local로 등록한 인증과정 실행
+    passport.authenticate('local', (passportError, user, info) => {
+      // 인증이 실패했거나 유저 데이터가 없다면 에러 발생
+      if (passportError || !user) {
+        res.status(400).json({ message: info.reason });
+        return;
+      }
+      // user데이터를 통해 로그인 진행
+      req.login(user, { session: false }, (loginError) => {
+        if (loginError) {
+          res.send(loginError);
+          return;
+        }
+        // 클라이언트에게 JWT생성 후 반환
+        const token = jwt.sign(
+          { id: user.id, email: user.email },
+          process.env.JWT_SECRET || 'jwt-secret-key',
+        );
+        res.json({ token });
+      });
+    })(req, res);
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+});
+
+router.post('/auth', mustLogedIn, async (req, res, next) => {
+  try {
+    res.json({ result: true });
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
 });
 
 export default router;
